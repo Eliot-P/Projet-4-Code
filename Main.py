a29213c22d2159dd3f747d0a2de2b3cd1c24857a
Eliot Peeters <eliot.peeters@student.uclouvain.be>
2022-03-29 15:42:01 +0200

    detection des angles

--- Main.py
+++ Main.py
@@ -28,9 +28,9 @@ def square_finder(x,y) :
     max = np.argmax(dxdy[100:])
     return dxdy,max
 
-def find_angles_rework(x,y):
-    m1 = np.polyfit(x[:50],y[:50],1)[0]
-    m2 = np.polyfit(x[-50:],y[-50:],1)[0]
+def find_angles_rework(xi,yi):
+    m1 = np.polyfit(xi[:10],yi[:10],1)[0]
+    m2 = np.polyfit(xi[-10:],yi[-10:],1)[0]
     theta = np.arctan((m1 - m2)/(1 + m1*m2))
     theta_refined = abs(theta)
     return theta_refined
@@ -44,10 +44,10 @@ def find_angle(x,y) :
     m1 = (x[1] - x[0])/(y[1] - y[0])
     m2 = (x[3] - x[2])/(y[3] - y[2])
     theta = np.arctan((m1 - m2)/(1 + m1*m2))
-    theta_refined = abs(theta)
+    theta_refined = (theta)
     return theta_refined
 
-C = coda.import_data("Data/GBIO_2022_Group_2_S2_20220007_009.TXT")
+C = coda.import_data("Data/GBIO_2022_Group_2_S2_20220007_008.TXT")
 #x = (C["Marker5_X"]).to_numpy()
 #y = (C["Marker5_Y"]).to_numpy()
 x = sp.filter_signal(C["Marker5_X"])
@@ -64,23 +64,55 @@ f = np.argsort(-np.array(angles))[:28]
 """
 
 t = np.array(C["time"])
-vx = sp.derive(sp.derive(x,200),200)
-vy = np.gradient(t,y)
-vmaxx = np.argmax(vx[40:-40])
-vmaxy = np.argmax(vy)
+vx = sp.derive(x,200)
+vy = sp.derive(y,200)
 
-fig = plt.figure()
-ax = plt.axes()
-dxdy,max = square_finder(x,y)
+def separator(x,y,vx,vy,t):
+    median = np.median(y[1000:5000])
+    max = np.argmax(y)
+    dist = y[max] - median
+    threshold = y[max]-2*dist
+    i = np.argwhere(y>threshold)[0][0]
+    sample_y = y[i-50:i+10]
+    sample_x = x[i-50:i+10]
+    angles = np.zeros_like(sample_x)
+    for j in range(len(sample_x) - 20) :
+        angles[j] = find_angles_rework(sample_x[j:j+20],sample_y[j:j+20])
+    f = np.argmax(-angles) +10
+    return f+i-50
 
-fig.subplot(2,1,1)
-ax.plot(t,x)
-ax.grid()
-fig.subplot(2,1,2)
-ax.plot(t,vx)
-ax.grid()
-#ax.set_ylim(-50,50)
-#ax.scatter(x[max],y[max],color='blue')
-#ax.scatter(x[vmaxx],y[vmaxx],color='green')
-#ax.scatter(x[f],y[f],color='red')
+median = np.median(y[1000:5000])
+max = np.argmax(y)
+dist = y[max] - median
+
+def animation(x,y,t):
+    for i in range(len(x)) :
+        if i ==0 : 
+            plt.subplot(1,2,1)
+            line1, =plt.plot(x[:],y[:])
+            plt.subplot(2,2,2)
+            line2, = plt.plot(t[:],x[:])
+            plt.subplot(2,2,4)
+            line3, = plt.plot(t[:],y[:])
+        else:
+            line1.set_data(x[:i],y[:i])
+            line2.set_data(t[:i],x[:i])
+            line3.set_data(t[:i],y[:i])
+        plt.pause(0.0001)
+    return
+#animation(x,y,t)
+f = separator(x,y,vx,vy,t)
+plt.plot(t,y)
+plt.axhline(y=median)
+plt.axhline(y=y[max])
+plt.axhline(y=y[max]-2*dist)
+plt.scatter(t[f],y[f])
+plt.grid()
+
+plt.show()
+
+
+
+plt.plot(x,y)
+plt.scatter(x[f],y[f])
 plt.show()
